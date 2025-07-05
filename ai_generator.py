852fe9b4be5a47bf1e77c1f39982b1904ca41f2b
Charudatta24 <pesala.govinda2023@vitstudent.ac.in>
2025-07-05 15:27:04 +0530

    Update ai_generator.py

--- ai_generator.py
+++ ai_generator.py
@@ -1,15 +1,7 @@
-# ai_generator.py
-
 import google.generativeai as genai
-
-# Set your Gemini API key
 API_KEY = "AIzaSyDRWRMg3Ui3oJX_mB4O10HL0ZDHYS3rZ_M"
-
 genai.configure(api_key=API_KEY)
-
-# Load the Gemini 2.0 Flash model
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-
 def get_gemini_response(prompt):
     try:
         response = model.generate_content(prompt)
